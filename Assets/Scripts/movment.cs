1860fec421dff8c499d5f0c3f327ec40c1527cb0
friedcheese21 <student66@maydm.org>
2024-07-25 10:40:36 -0500

    ff
    ff

--- Assets/Scripts/movment.cs
+++ Assets/Scripts/movment.cs
@@ -2,49 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class movment : MonoBehaviour 
-{ 
-
+public class movment : MonoBehaviour
+{
     public Rigidbody2D myRigidbody2d;
     public SpriteRenderer mySpriteRenderer;
-   // public AudioSource myAudioSource;
+    // public AudioSource myAudioSource;
     public int moveSpeed = 0;
     public float jumpSpeed;
+
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashingPower = 24f;
+    public float dashingTime = 2f;
+    //private float dashingCooldown = 1f;
+    public characterRespawn spawnpoint;
 
     public Transform groundCheckPoint;
     private bool onGround;
 
+    [SerializeField] private TrailRenderer tr;
     // Start is called before the first frame update
-    void Start() {
-        
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
 
         Vector2 currentVelocity = myRigidbody2d.velocity;
         Vector2 newVelocity = currentVelocity;
 
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            Debug.Log("Player Dashing");
+            StartCoroutine(Dash());
+        }
+
         bool pressedRight = Input.GetKey(KeyCode.RightArrow);
         bool pressedLeft = Input.GetKey(KeyCode.LeftArrow);
 
-        if (pressedRight) {
+        if (pressedRight)
+        {
 
             //myAudioSource.Play();
             transform.eulerAngles = new Vector3(0f, 0f, 0f);
             newVelocity.x = moveSpeed;
-         //   mySpriteRenderer.flipX = false;
+            //   mySpriteRenderer.flipX = false;
 
         }
-        
+
         //myAudioSource.Stop();
 
         if (pressedLeft)
         {
             transform.eulerAngles = new Vector3(0f, 180f, 0f);
             newVelocity.x = -moveSpeed;
-          //  mySpriteRenderer.flipX = true;
+            //  mySpriteRenderer.flipX = true;
         }
 
         myRigidbody2d.velocity = newVelocity;
@@ -52,7 +72,7 @@ public class movment : MonoBehaviour
         onGround = IsOnGround();
 
         bool pressedJump = Input.GetKeyDown(KeyCode.Space);
-        if(pressedJump && onGround)
+        if (pressedJump && onGround)
         {
             Jump(jumpSpeed);
 
@@ -75,7 +95,34 @@ public class movment : MonoBehaviour
 
     public void Die()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        transform.position = spawnpoint.checkpointFlag;
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = myRigidbody2d.gravityScale;
+        myRigidbody2d.gravityScale = 0f;
+        
+        myRigidbody2d.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        myRigidbody2d.gravityScale = originalGravity;
+        isDashing = false;
+        canDash = true;
+    }
+
+    private void FixedUpdate()
+    {
+        //Debug.Log("rb velocity is " + myRigidbody2d.velocity);
+        if (isDashing)
+        {
+            return;
+        }
     }
 }
 
